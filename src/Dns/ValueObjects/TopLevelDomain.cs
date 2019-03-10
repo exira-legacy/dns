22902effafc143fafbd9c5ee28ad596d8050687f
David Cumps <david@cumps.be>
2019-03-10 18:23:39 +0100

    feat: add more top level domains

--- src/Dns/ValueObjects/TopLevelDomain.cs
+++ src/Dns/ValueObjects/TopLevelDomain.cs
@@ -15,9 +15,31 @@ namespace Dns
         [SuppressMessage("ReSharper", "InconsistentNaming")]
         public static readonly TopLevelDomain
             be = new TopLevelDomain("be", ".be", "Belgium"),
+            biz = new TopLevelDomain("biz", ".biz", "Business"),
+            brussels = new TopLevelDomain("brussels", ".brussels", "Brussels"),
+            cn = new TopLevelDomain("cn", ".cn", "China"),
+            com = new TopLevelDomain("com", ".com", "Commercial"),
+            consulting = new TopLevelDomain("consulting", ".consulting", "Consulting"),
+            design = new TopLevelDomain("design", ".design", "Design"),
+            domains = new TopLevelDomain("domains", ".domains", "Domains"),
+            es = new TopLevelDomain("es", ".es", "Spain"),
             eu = new TopLevelDomain("eu", ".eu", "Europe"),
+            fr = new TopLevelDomain("fr", ".fr", "France"),
+            gent = new TopLevelDomain("gent", ".gent", "Ghent"),
+            ie = new TopLevelDomain("ie", ".ie", "Republic of Ireland"),
+            @in = new TopLevelDomain("in", ".in", "India"),
+            info = new TopLevelDomain("info", ".info", "Information"),
+            io = new TopLevelDomain("io", ".io", "British Indian Ocean Territory"),
+            lu = new TopLevelDomain("lu", ".lu", "Luxembourg"),
+            me = new TopLevelDomain("me", ".me", "Montenegro"),
+            mobi = new TopLevelDomain("mobi", ".mobi", "Mobile"),
+            net = new TopLevelDomain("net", ".net", "Network"),
             nl = new TopLevelDomain("nl", ".nl", "The Netherlands"),
-            com = new TopLevelDomain("com", ".com", "Commercial");
+            nu = new TopLevelDomain("nu", ".nu", "Niue"),
+            one = new TopLevelDomain("one", ".one", "One"),
+            org = new TopLevelDomain("org", ".org", "Organisation"),
+            se = new TopLevelDomain("se", ".se", "Sweden"),
+            vlaanderen = new TopLevelDomain("vlaanderen", ".vlaanderen", "Flanders");
 
         private TopLevelDomain(
             string extension,
